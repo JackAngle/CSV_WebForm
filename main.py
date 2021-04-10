152427b38113ebd0bce97e14f5dbe35120412875
JackAngle156 <lehuythinh156@gmail.com>
2021-04-10 16:16:12 +0700

    First time, the code fully works

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import requests
+import os
 
 WEBFORM_URL = 'http://127.0.0.1:8000/add_topic/'
 
@@ -8,16 +9,17 @@ with open('file.csv', newline='') as csvfile:
     data = (dict(spamreader))
 
     # Edit data
-    data = dict((k.lower(), v) for k, v in data.items()) # change all keys to lowercase
+    data = dict((k.lower(), v) for k, v in data.items())  # change all keys to lowercase
     # Change topic_img key to image
     try:
         data['image'] = data.pop('topic_img')
     except KeyError:
         print("KeyError: key is not exist to be changed")
 
-    # Change topic_img key to image
     try:
-        data = dict((k.replace(' ', '_'), v) for k, v in data.items())
+        data = dict((k.replace(' ', '_'), v) for k, v in data.items())  # Replace space in keys with '_'
+        data['image'] = dir_path = os.path.dirname(os.path.realpath(__file__)) + data['image']
+        data.pop('image')
     except KeyError:
         print("KeyError: key is not exist to be changed")
 
@@ -30,7 +32,7 @@ with open('file.csv', newline='') as csvfile:
     }
 
     client = requests.Session()
-    client.get(WEBFORM_URL) #sets_cookies
+    client.get(WEBFORM_URL)  # sets_cookies
 
     if 'csrftoken' in client.cookies:
         # Django 1.6 and up
@@ -46,7 +48,9 @@ with open('file.csv', newline='') as csvfile:
     # resp = requests.get(WEBFORM_URL)
     # print("Response to GET request: %s" % resp.content)
     # login_data = dict(username=EMAIL, password=PASSWORD, csrfmiddlewaretoken=csrftoken, next='/')
-    client = client.post(WEBFORM_URL, data=data, headers=dict(Referer=WEBFORM_URL))
+
+    files = {'image': open('smith.jpg', 'rb')}
+    client = client.post(WEBFORM_URL, data=data, headers=dict(Referer=WEBFORM_URL), files=files)
     print("Headers from a POST request response: %s" % client.headers)
     print("HTML Response: %s" % client.text)
 
